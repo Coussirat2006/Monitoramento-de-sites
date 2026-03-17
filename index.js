791b486400a874cf0dd8f93515e4373a966f85c7
Coussirat2006 <lukascoussirat@gmail.com>
2026-03-17 14:41:07 -0300

    Adicionando a API e a primeira versão do Frontend

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs'); // 1. Pegamos a ferramenta de escrever arquivos
-
+const express = require('express');
+const app = express();
+const porta = 3000;
+app.use(express.static('public'));
 const sites = [
     { nome: "Google", categoria: "Redes", url: "https://www.google.com" },
     { nome: "GitHub", url: "https://github.com" },
@@ -23,15 +26,18 @@ async function monitorar() {
             const resposta = await fetch(site.url);
 
             if (resposta.ok) {
+                site.status = "ONLINE";
                 const msg = `✅ ${site.nome}: ONLINE (${resposta.status})`;
                 console.log(msg); // Mostra na tela
                 salvarNoLog(msg); // <--- AQUI NÓS CHAMAMOS A RECEITA PARA SALVAR NO ARQUIVO!
             } else {
+                site.status = "PROBLEMA";
                 const msg = `⚠️ ${site.nome}: PROBLEMA (Status: ${resposta.status})`;
                 console.log(msg);
                 salvarNoLog(msg); // <--- SALVA NO ARQUIVO TAMBÉM
             }
         } catch (erro) {
+            site.status = "OFFLINE";
             const msg = `❌ ${site.nome}: OFFLINE (Erro na conexão)`;
             console.log(msg);
             salvarNoLog(msg); // <--- SALVA NO ARQUIVO TAMBÉM
@@ -40,9 +46,18 @@ async function monitorar() {
     console.log("-------------------------------\n");
 }
 
+// Rota da API (O "Guichê" de atendimento)
+app.get('/api/status', (req, res) => {
+    res.json(sites); // Quando o navegador acessar, enviamos a lista de sites
+});
+
 const TEMPO_ESPERA = 5000; 
 
-console.log(`🚀 Monitor iniciado! Verificando a cada ${TEMPO_ESPERA / 1000} segundos.`);
-
-monitorar(); // Roda a primeira vez
-setInterval(monitorar, TEMPO_ESPERA); // Roda a cada 5 segundos
+// Ligando a máquina do servidor
+app.listen(porta, () => {
+    console.log(`\n🚀 Servidor API ligado com sucesso!`);
+    console.log(`Acesse no navegador: http://localhost:${porta}/api/status\n`);
+    
+    monitorar(); // Dá o primeiro "ping" nos sites
+    setInterval(monitorar, TEMPO_ESPERA); // Continua a cada 5 segundos
+});
